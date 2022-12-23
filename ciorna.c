ebeca44034d92a6da6a22c93a03b5bd7bdf936ed
Marina Oprea <marina.oprea2302@gmail.com>
2022-12-23 19:08:37 +0200

    added colored images

--- ciorna.c
+++ ciorna.c
@@ -101,6 +101,31 @@ unsigned char **alloc_matrix(int height, int width)
     return a;
 }
 
+colored_image **alloc_matrix_color(int height, int width)
+{
+    colored_image **a;
+    a = malloc(height * sizeof(colored_image *));
+    if (a == NULL) {
+        fprintf(stderr, "malloc() failed\n");
+        return NULL;
+    }
+
+    for (int i = 0; i < height; i++) {
+        a[i] = malloc(width * sizeof(colored_image));
+        if (a[i] == NULL) {
+            fprintf(stderr, "malloc() failed\n");
+            i--;
+            while (i >= 0) {
+                free(a[i]);
+                i--;
+            }
+            free(a);
+            return NULL;
+        }
+    }
+    return a;
+}
+
 unsigned char **load_bw(unsigned char *filename, int type, int *height, int *width) {
     unsigned char **image;
 
@@ -140,6 +165,51 @@ unsigned char **load_bw(unsigned char *filename, int type, int *height, int *wid
     return image;
 }
 
+colored_image **load_color(unsigned char *filename, int type, int *height, int *width)
+{
+    colored_image **image;
+
+    if (type%10 == 0) {
+        FILE *in = fopen(filename, "rt");
+        if (in == NULL)
+            return NULL;
+        unsigned char c;
+        int auxr, auxg, auxb;
+        fscanf(in, "P%c%d%d%d\n", &c, width, height, &auxr);
+        image = alloc_matrix_color(*height, *width);
+        if (image == NULL)
+            return NULL;
+        for (int i = 0; i < *height; i++)
+            for (int j = 0; j < *width; j++) {
+                fscanf(in, "%d%d%d", &auxr, &auxg, &auxb);
+                image[i][j].R = (unsigned char) auxr;
+                image[i][j].G = (unsigned char) auxg;
+                image[i][j].B = (unsigned char) auxb;
+            }
+        fclose(in);
+    } else {
+        FILE *in = fopen(filename, "rb");
+        if (in == NULL)
+            return NULL;
+        int aux;
+        unsigned char *aux2 = malloc(2 * sizeof(unsigned char));
+        fscanf(in, "%s%d%d%d", aux2, width, height, &aux);
+        image = alloc_matrix_color(*height, *width);
+        if (image == NULL)
+            return NULL;
+        for (int i = 0; i < *height; i++)
+            for (int j = 0; j < *width; j++) {
+                fread(&(image[i][j].R), sizeof(unsigned char), 1, in);
+                fread(&(image[i][j].G), sizeof(unsigned char), 1, in);
+                fread(&(image[i][j].B), sizeof(unsigned char), 1, in);
+            }
+        free(aux2);
+        fclose(in);
+    }
+
+    return image;
+}
+
 void free_matrix_bw(unsigned char **a, int m)
 {
     for (int i = 0; i < m; i++)
@@ -154,7 +224,6 @@ void free_matrix_color(colored_image **a, int m)
     free(a);
 }
 
-
 void load_cmd(unsigned char *filename, unsigned char ***im_bw, unsigned char ***im_gray, colored_image ***im_color, int *height, int *width)
 {
     int type = image_type(filename);
@@ -165,7 +234,8 @@ void load_cmd(unsigned char *filename, unsigned char ***im_bw, unsigned char ***
         if (*im_gray != NULL)
             free_matrix_bw(*im_gray, *height);
         else
-            free_matrix_color(*im_color, *height);
+            if (*im_color != NULL)
+                free_matrix_color(*im_color, *height);
 
     if (type < 10) {
         *im_bw = load_bw(filename, type, height, width);
@@ -181,9 +251,13 @@ void load_cmd(unsigned char *filename, unsigned char ***im_bw, unsigned char ***
                 printf("Failed to load %s\n", filename);
                 return;
             }
+        } else {
+            *im_color = load_color(filename, type, height, width);
+            if (*im_color == NULL) {
+                printf("Failed to load %s\n", filename);
+                return;
+            }
         }
-        // else
-        //   *im_color = load_color(filename, type, &height, &width);
     }
     printf("Loaded %s\n", filename);
 }
@@ -220,6 +294,31 @@ void save_gray(unsigned char *filename, unsigned char **im, int height, int widt
     }
 }
 
+void save_color(unsigned char *filename, colored_image **im, int height, int width, int ascii)
+{
+    if (ascii == 1) {
+        FILE *out = fopen(filename, "wt");
+        fprintf(out, "P3\n%d %d\n", width, height);
+        fprintf(out, "255\n");
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++)
+                fprintf(out, "%hhu %hhu %hhu", im[i][j].R, im[i][j].G, im[i][j].B);
+            fprintf(out, "\n");
+        }
+        fclose(out);
+    } else {
+        FILE *out = fopen(filename, "w");
+        fprintf(out, "P6\n%d %d\n255\n", width, height);
+        for (int i = 0; i < height; i++)
+            for (int j = 0; j < width; j++) {
+                fwrite(&(im[i][j].R), sizeof(unsigned char), 1, out);
+                fwrite(&(im[i][j].G), sizeof(unsigned char), 1, out);
+                fwrite(&(im[i][j].B), sizeof(unsigned char), 1, out);
+            }
+        fclose(out);
+    }
+}
+
 void save_cmd(unsigned char **line, unsigned char **im_bw, unsigned char **im_gray, colored_image **im_color, int height, int width)
 {
     if (im_bw == NULL && im_gray == NULL && im_color == NULL) {
@@ -244,8 +343,8 @@ void save_cmd(unsigned char **line, unsigned char **im_bw, unsigned char **im_gr
     else*/
         if (im_gray)
             save_gray(filename, im_gray, height, width, ascii);
-            /*else
-                save_color(filename, im_color, height, width, ascii);*/
+            else
+                save_color(filename, im_color, height, width, ascii);
     printf("Saved %s\n", filename);
 }
 
